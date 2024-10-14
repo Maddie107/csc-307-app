539e1a115f76e67cae2cf1da606580a3fde63d48
Maddie107 <connellyrogers@gmail.com>
2024-10-14 11:58:20 -0700

    Step 3 of IE3
    Added cors to backend.js and added fetch user and update list. Connected front and backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,5 +1,5 @@
 // src/MyApp.jsx (a new function inside the MyApp function)
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
@@ -7,23 +7,52 @@ function MyApp() {
 	const [characters, setCharacters] = useState([]);
 	function removeOneCharacter(index) {
 		const updated = characters.filter((character, i) => {
-		  return i !== index;
+			return i !== index;
 		});
 		setCharacters(updated);
 	}
-	function updateList(person) {
-		setCharacters([...characters, person]);
+	// function updateList(person) {
+	// 	setCharacters([...characters, person]);
+	// }
+	function fetchUsers() {
+		const promise = fetch("http://localhost:8000/users");
+		return promise;
 	}
+	useEffect(() => {
+		fetchUsers()
+			.then((res) => res.json())
+			.then((json) => setCharacters(json["users_list"]))
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
+	function postUser(person) {
+		const promise = fetch("Http://localhost:8000/users", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(person)
+		});
 
-  return (
-    <div className="container">
-       <Table 
-	   characterData={characters} 
-	   removeCharacter={removeOneCharacter}
-	   />
-	    <Form handleSubmit={updateList} />
-    </div>
-  );
+		return promise;
+	}
+	function updateList(person) {
+		postUser(person)
+		  .then(() => setCharacters([...characters, person]))
+		  .catch((error) => {
+			console.log(error);
+		  });
+	  }
+	return (
+		<div className="container">
+			<Table
+				characterData={characters}
+				removeCharacter={removeOneCharacter}
+			/>
+			<Form handleSubmit={updateList} />
+		</div>
+	);
 }
 
 export default MyApp;
